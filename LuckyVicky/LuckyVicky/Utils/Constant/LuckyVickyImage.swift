1b1d1271762a9ab8e3c7183bde01be713613e222
eunjuchoi <eunjuzzoo@naver.com>
2024-07-23 15:25:40 +0900

    add:LuckyVickyImage 열거형 및 Image+ 추가 (#3)

---
+++ LuckyVicky/LuckyVicky/Utils/Constant/LuckyVickyImage.swift
@@ -0,0 +1,15 @@
+//
+//  LuckyVickyImage.swift
+//  LuckyVicky
+//
+//  Created by EUNJU on 7/22/24.
+//
+
+import Foundation
+
+enum LuckyVickyImage: String {
+    case wonyoung
+    case heejin
+    case woohee
+    case heungmin
+}
