5d074e7e3033c09fd73d55e64bd36ddb2e8814e7
eunjuchoi <eunjuzzoo@naver.com>
2024-07-28 13:09:03 +0900

    feat: 캐릭터셀 선택 이벤트 처리 (#11)
    - 셀 배경 컬러 변경
    - 선택하기 버튼 활성화

--- LuckyVicky/LuckyVicky/Presentation/SelectCharacterView.swift
+++ LuckyVicky/LuckyVicky/Presentation/SelectCharacterView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct SelectCharacterView: View {
+    @State private var selectedId: Int?
+    
+    private var characters: [CharacterEntity] = [
+        CharacterEntity(id: 0, name: "원영", introduction: "완전 럭키비키잖앙~", imageName: .wonyoung),
+        CharacterEntity(id: 1, name: "희진", introduction: "맞다이로 들어와", imageName: .heejin),
+        CharacterEntity(id: 2, name: "우희", introduction: "얼마나 잘 되려고 이럴까?", imageName: .woohee),
+        CharacterEntity(id: 3, name: "흥민", introduction: "그냥 좋다고 생각하면 돼.", imageName: .heungmin)
+    ]
     
     var body: some View {
         ZStack {
@@ -29,26 +37,28 @@ struct SelectCharacterView: View {
                     .padding(.leading, 28)
                     Spacer()
                 }
-
+                
                 Spacer()
-                LazyVStack {
-                    characterCell(entity: CharacterEntity(id: 0, name: "원영", introduction: "완전 럭키비키잖앙~", imageName: .wonyoung))
-                    characterCell(entity: CharacterEntity(id: 1, name: "희진", introduction: "맞다이로 들어와", imageName: .heejin))
-                    characterCell(entity: CharacterEntity(id: 2, name: "우희", introduction: "얼마나 잘 되려고 이럴까?", imageName: .woohee))
-                    characterCell(entity: CharacterEntity(id: 3, name: "흥민", introduction: "그냥 좋다고 생각하면 돼.", imageName: .heungmin))
-                    
+                VStack(spacing: 15) {
+                    ForEach(characters, id: \.self) { character in
+                        characterCell(entity: character)
+                            .onTapGesture {
+                                selectedId = character.id
+                            }
+                    }
                 }
                 .padding(.horizontal, 29)
                 
                 Spacer()
                 LuckyVickyButton(title: "선택하기",
+                                 isActive: selectedId != nil,
                                  action: {})
                 .padding(.horizontal, 22)
             }
         }
     }
     
-    @ViewBuilder 
+    @ViewBuilder
     func characterCell(entity: CharacterEntity) -> some View {
         
         HStack(alignment: .center) {
@@ -70,7 +80,7 @@ struct SelectCharacterView: View {
             Spacer()
         }
         .frame(maxWidth: .infinity)
-        .background(.white)
+        .background(selectedId == entity.id ? .mainGreen : .white)
         .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
