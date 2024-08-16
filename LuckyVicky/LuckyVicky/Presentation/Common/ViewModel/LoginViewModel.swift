d00c5daa1233cb481494674a12fa8964b506bcb4
eunjuchoi <eunjuzzoo@naver.com>
2024-08-16 21:19:07 +0900

    feat: 로그인 기능 구현 (#20)

--- LuckyVicky/LuckyVicky/Presentation/Common/ViewModel/LoginViewModel.swift
+++ LuckyVicky/LuckyVicky/Presentation/Common/ViewModel/LoginViewModel.swift
@@ -6,39 +6,72 @@
 //
 
 import Foundation
+import AuthenticationServices
+import Combine
 
 final class LoginViewModel: ViewModelable {
     @Published var state: State
+    private let useCase: LoginUseCase
+    private var cancellables = Set<AnyCancellable>()
+    private var currentNonce: String?
     
-    init() {
-        self.state = State(isPresented: false, 
+    init(useCase: LoginUseCase) {
+        self.state = State(isLoading: false,
+                           isPresented: false,
                            hasErrorOccurred: false)
+        self.useCase = useCase
     }
     
     enum Action {
-        case onTapLoginButton
+        case onTapAppleLoginButton(request: ASAuthorizationAppleIDRequest)
+        case onCompletedAuthorization(result: Result<ASAuthorization, Error>)
     }
     
     struct State {
+        var isLoading: Bool
         var isPresented: Bool
         var hasErrorOccurred: Bool
     }
     
     func action(_ action: Action) {
         switch action {
-        
-        case .onTapLoginButton:
-            state.isPresented = true
-            login()
+        case .onTapAppleLoginButton(let request):
+            state.isLoading = true
+            let nonce = useCase.executeAuthorization(request)
+            currentNonce = nonce
+        case .onCompletedAuthorization(let result):
+            login(result)
         }
     }
 }
 
 extension LoginViewModel {
     
-    private func login() {
+    private func login(_ result: Result<ASAuthorization, Error>) {
+        if case let .success(authorization) = result {
+            guard let nonce = currentNonce else { return }
+            
+            useCase.executeSignIn(authorization, nonce: nonce)
+                .sink { [weak self] completion in
+                    if case .failure = completion {
+                        self?.state.isLoading = false
+                    }
+                } receiveValue: { [weak self] user in
+                    self?.updateStateAfterLoginSuccess(user: user)
+                }.store(in: &cancellables)
+      
+        } else if case let .failure(error) = result {
+            state.isLoading = false
+            state.hasErrorOccurred = true
+        }
+    }
+    
+    private func updateStateAfterLoginSuccess(user: UserEntity) {
+        state.isLoading = false
+        UserDefaults.userId = user.id
+        UserDefaults.usedCount = user.usedCount
+        state.isPresented = true
         UserDefaults.isFirstLaunch = false
-        // TODO: 로그인 및 초기 데이터 세팅 (오늘 사용 가능 횟수)
     }
 }
 
