76fa00711693b0cb937b52492e886e3485f24098
eunjuchoi <eunjuzzoo@naver.com>
2024-08-27 17:03:33 +0900

    refactor: repository 레이어 코드 리팩토링 (#46)
    service의 api 이용해 통신하고 domain 레이어에 맞도록 entity로 변경

--- LuckyVicky/LuckyVicky/Data/Repositories/SignRepository.swift
+++ LuckyVicky/LuckyVicky/Data/Repositories/SignRepository.swift
@@ -2,165 +2,44 @@
 //  SignRepository.swift
 //  LuckyVicky
 //
-//  Created by EUNJU on 8/13/24.
+//  Created by EUNJU on 8/26/24.
 //
 
 import Foundation
 import AuthenticationServices
-import CryptoKit
 import Combine
-
-import FirebaseAuth
-
-enum AuthenticationError: Error {
-    case clientIDError
-    case tokenError
-    case invalidated
-}
 
 protocol SignRepository {
     func checkAuthenticationState() -> String?
-    func handleSignInWithAppleRequest(_ request: ASAuthorizationAppleIDRequest) -> String
-    func handleSignInWithAppleCompletion(_ authorization: ASAuthorization, none: String) -> AnyPublisher<UserDTO, AuthenticationError>
+    func requestAuthorization(_ request: ASAuthorizationAppleIDRequest) -> String
+    func signIn(_ authorization: ASAuthorization, nonce: String) -> AnyPublisher<UserEntity, NetworkError>
 }
 
 final class SignRepositoryImpl: SignRepository {
-
+    
+    private let service: SignService
+    
+    init(service: SignService) {
+        self.service = service
+    }
+    
     func checkAuthenticationState() -> String? {
-        if let user = Auth.auth().currentUser {
-            return user.uid 
-        } else {
-            return nil
-        }
+        return service.checkAuthenticationState()
     }
     
-    func handleSignInWithAppleRequest(
-        _ request: ASAuthorizationAppleIDRequest
-    ) -> String {
-        request.requestedScopes = [.fullName, .email]
-        let nonce = randomNonceString()
-        request.nonce = sha256(nonce)
-        return nonce
+    func requestAuthorization(_ request: ASAuthorizationAppleIDRequest) -> String {
+        return service.handleSignInWithAppleRequest(request)
     }
     
-    func handleSignInWithAppleCompletion(
-        _ authorization: ASAuthorization,
-        none: String
-    ) -> AnyPublisher<UserDTO, AuthenticationError> {
-        Future { [weak self] promise in
-            self?.handleSignInWithAppleCompletion(authorization, nonce: none) { result in
-                switch result {
-                case let .success(user):
-                    promise(.success(user))
-                case let .failure(error):
-                    promise(.failure(error))
-                }
+    func signIn(_ authorization: ASAuthorization, 
+                nonce: String
+    ) -> AnyPublisher<UserEntity, NetworkError> {
+        service.handleSignInWithAppleCompletion(authorization, none: nonce)
+            .mapError { _ in
+                NetworkError.unauthorized
             }
-        }.eraseToAnyPublisher()
+            .map { $0.toEntity() }
+            .eraseToAnyPublisher()
     }
 }
 
-extension SignRepositoryImpl {
-    
-    private func randomNonceString(length: Int = 32) -> String {
-        precondition(length > 0)
-        let charset: [Character] =
-        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        var result = ""
-        var remainingLength = length
-        
-        while remainingLength > 0 {
-            let randoms: [UInt8] = (0 ..< 16).map { _ in
-                var random: UInt8 = 0
-                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                if errorCode != errSecSuccess {
-                    fatalError(
-                        "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
-                    )
-                }
-                return random
-            }
-            
-            randoms.forEach { random in
-                if remainingLength == 0 {
-                    return
-                }
-                
-                if random < charset.count {
-                    result.append(charset[Int(random)])
-                    remainingLength -= 1
-                }
-            }
-        }
-        
-        return result
-    }
-    
-    private func sha256(_ input: String) -> String {
-        let inputData = Data(input.utf8)
-        let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap {
-            String(format: "%02x", $0)
-        }.joined()
-        
-        return hashString
-    }
-    
-    private func handleSignInWithAppleCompletion(
-        _ authorization: ASAuthorization,
-        nonce: String,
-        completion: @escaping (Result<UserDTO, AuthenticationError>) -> Void
-    ) {
-        guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential,
-              let appleIDToken = appleIDCredential.identityToken 
-        else {
-            completion(.failure(AuthenticationError.tokenError))
-            return
-        }
-        
-        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) 
-        else {
-            completion(.failure(AuthenticationError.tokenError))
-            return
-        }
-        
-        let credential = OAuthProvider.appleCredential(withIDToken: idTokenString,
-                                                       rawNonce: nonce,
-                                                       fullName: appleIDCredential.fullName)
-        
-        authenticateUserWithFirebase(credential: credential) { result in
-            switch result {
-            case let .success(user):
-                completion(.success(user))
-            case let .failure(error):
-                completion(.failure(AuthenticationError.clientIDError))
-            }
-        }
-    }
-    
-    private func authenticateUserWithFirebase(
-        credential: AuthCredential,
-        completion: @escaping (Result<UserDTO, Error>) -> Void
-    ) {
-        Auth.auth().signIn(with: credential) { result, error in
-            if let error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard let result else {
-                completion(.failure(AuthenticationError.invalidated))
-                return
-            }
-            
-            let firebaseUser = result.user
-            
-            let user: UserDTO = .init(id: firebaseUser.uid,
-                                      name: firebaseUser.displayName ?? "",
-                                      usedCount: 0,
-                                      lastUsedDate: "")
-            
-            completion(.success(user))
-        }
-    }
-}
---
+++ LuckyVicky/LuckyVicky/Data/Repositories/UserRepository.swift
@@ -0,0 +1,79 @@
+//
+//  UserRepository.swift
+//  LuckyVicky
+//
+//  Created by EUNJU on 8/26/24.
+//
+
+import Foundation
+import Combine
+
+protocol UserRepository {
+    func createUser(_ dto: UserDTO) -> AnyPublisher<Void, NetworkError>
+    func fetchUserData(userId: String) -> AnyPublisher<UserEntity, NetworkError>
+    func updateUserData(userId: String, lastUsedDate: String, usedCount: Int) -> AnyPublisher<Void, NetworkError>
+    func deleteUserData(userId: String) -> AnyPublisher<Void, NetworkError>
+}
+
+final class UserRepositoryImpl: UserRepository {
+    
+    private var service: FirebaseDBService
+    private let dbKey = "Users"
+    
+    init(service: FirebaseDBService) {
+        self.service = service
+    }
+    
+    func createUser(_ dto: UserDTO) -> AnyPublisher<Void, NetworkError> {
+        Just(dto)
+            .compactMap { try? JSONEncoder().encode($0) }
+            .compactMap { try? JSONSerialization.jsonObject(with: $0, options: .fragmentsAllowed) }
+            .flatMap { [weak self] value -> AnyPublisher<Void, DBError> in
+                guard let self = self else { return Empty().eraseToAnyPublisher() }
+                return self.service.create(key: dbKey, path: dto.id, value: value)
+            }
+            .mapError { _ in NetworkError.serverError }
+            .eraseToAnyPublisher()
+    }
+    
+    func fetchUserData(userId: String) -> AnyPublisher<UserEntity, NetworkError> {
+        service.fetch(key: dbKey, path: userId)
+            .mapError { _ in NetworkError.serverError}
+            .flatMap { value in
+                if let value {
+                    return Just(value)
+                        .tryMap { try JSONSerialization.data(withJSONObject: $0) }
+                        .decode(type: UserDTO.self, decoder: JSONDecoder())
+                        .map { $0.toEntity() }
+                        .mapError { _ in NetworkError.serverError }
+                        .eraseToAnyPublisher()
+                } else {
+                    return Empty().eraseToAnyPublisher()
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func updateUserData(
+        userId: String,
+        lastUsedDate: String,
+        usedCount: Int
+    ) -> AnyPublisher<Void, NetworkError> {
+        let data: [String: Any] = [
+            "lastUsedDate": lastUsedDate,
+            "usedCount": usedCount
+        ]
+        
+        return service.update(key: dbKey, path: userId, value: data)
+            .mapError { _ in NetworkError.serverError }
+            .eraseToAnyPublisher()
+    }
+    
+    func deleteUserData(userId: String) -> AnyPublisher<Void, NetworkError> {
+        service.delete(key: dbKey, path: userId)
+            .mapError{ _ in
+                return .serverError
+            }
+            .eraseToAnyPublisher()
+    }
+}
