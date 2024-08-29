33a6b700c4bac319a8fb1b2a1608736618bd88d4
eunjuchoi <eunjuzzoo@naver.com>
2024-08-30 01:09:59 +0900

    refactor: SplashView -> LaunchScreen 방식으로 수정 (#37)

--- LuckyVicky/LuckyVicky/Application/AppDelegate.swift
+++ LuckyVicky/LuckyVicky/Application/AppDelegate.swift
@@ -13,6 +13,7 @@ import FirebaseCore
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+      Thread.sleep(forTimeInterval: 1)
       FirebaseApp.configure()
       
       return true
